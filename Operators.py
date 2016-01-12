9967c77297888d4babd5e8f8d80e5805a4489131
Paul Fox <paul.fox@pgitl.com>
2016-01-12 11:46:13 +0000

    Slight refactoring of Variables to improve null safey/unassigned variable fn, and added examples to Operators.

---
+++ Operators.py
@@ -0,0 +1,74 @@
+#Operators and Precedence Examples and Exercises
+smallInt = 5;
+largeInt = 5000;
+
+def basicOperators():
+    #Basic Maths
+    #Addition
+    operation = str(largeInt) + '+' + str(smallInt)
+    result = eval(operation)
+    print(operation + ' = ' + str(result))
+    print('\n')
+    #Subtraction
+    operation = str(smallInt) + '-' + str(largeInt)
+    result = eval(operation)
+    print(operation + ' = ' + str(result))
+    print('\n')
+    #Multiplication
+    operation = str(smallInt) + '*' + str(largeInt)
+    result = eval(operation)
+    print(operation + ' = ' + str(result))
+    print('\n')
+    #Division
+    operation = str(largeInt) + '/' + str(smallInt)
+    result = eval(operation)
+    print(operation + ' = ' + str(result))
+    print('\n')
+    #Modulo
+    operation = str(smallInt) + '%' + str(largeInt)
+    result = eval(operation)
+    print(operation + ' = ' + str(result))
+    print('\n')
+
+def relationalOperators():
+    # Build a series of relational operations, execute and print the results
+    print('Relational')
+    smallInt = '5'
+    largeInt = '5000'
+    operators = [ '<','>','==','!=','>=','<=']
+    for op in operators:
+        operation = smallInt + op + largeInt
+        result = eval(operation)
+        print(operation + ' = ' + str(result))
+    print('\n')
+
+def logicalOperators():
+    # Build a series of logical operations, execute and print the results
+    print('Logical')
+    trueVar = 'True'
+    falseVar = 'False'
+    operators = [ 'and','or','and not']
+    for op in operators:
+        operation = '(trueVar ' + op + ' falseVar == True)'
+        result = eval(operation)
+        print(operation + ' = ' + str(result))
+    print('\n')
+
+def assignmentOperators():
+    # Build a series of assignment operations, execute and print the results
+    print('Assignment')
+    target = 15
+    source = 5
+    operators = ['+=', '*=', '-=', '/=', '%=', '**=', '//=']
+    print('target = ' + str(target))
+    for op in operators:
+        operation = 'target' + op + str(source)
+        print(operation)
+        exec(operation)
+        print('target = ' + str(target) +'\n')
+    print('\n')
+
+basicOperators()
+relationalOperators()
+logicalOperators()
+assignmentOperators()
--- Variables.py
+++ Variables.py
@@ -1,76 +1,94 @@
-# Example variable assignment
-a = 10
-print(a)
+def varAssign():
+    # Example variable assignment
+    a = 10
+    print(a)
 
-# Data types Exercise
-# Integer
-a = 10
-print(a)
-# Float
-a = 10.537
-print(a)
-# Boolean
-a = "False"
-print(a)
-# Interesting discussion point here to test for 'False' and have it output 'True'
-print(a == 'False')
-# String
-a = "string value"
-print(a)
-# Dict
-a = {'name': 'Paul', 'location': 'Bristol'}
-print(a)
-print(a['location'])
-print(a['name'])
+def dataTypes():
+    # Data types Exercise
+    # Integer
+    a = 10
+    print(a)
+    # Float
+    a = 10.537
+    print(a)
+    # Boolean
+    a = "False"
+    print(a)
+    # Interesting discussion point here to test for 'False' and have it output 'True'
+    print(a == 'False')
+    # String
+    a = "string value"
+    print(a)
+    # Dict
+    a = {'name': 'Paul', 'location': 'Bristol'}
+    print(a)
+    print(a['location'])
+    print(a['name'])
 
-# Type conversion example
-print('\nType conversion example')
-varToConvert = '10'
-convertedVar = int(varToConvert)
-print('varToConvert')
-print(varToConvert)
-print('convertedVar')
-print(convertedVar)
-try:
-    # Try to concatenate the str and the int. Exception will be caught.
-    print('varToConvert + convertedVar')
-    print(varToConvert + convertedVar)
-except TypeError as error:
-    print('An error occurred :\n' + str(error))
-finally:
+def typeConversion():
+    # Type conversion example
+    print('\nType conversion example')
+    varToConvert = '10'
+    convertedVar = int(varToConvert)
+    print('varToConvert')
+    print(varToConvert)
+    print('convertedVar')
+    print(convertedVar)
+    try:
+        # Try to concatenate the str and the int. Exception will be caught.
+        print('varToConvert + convertedVar')
+        print(varToConvert + convertedVar)
+    except TypeError as error:
+        print('An error occurred :\n' + str(error))
+    finally:
+        print('\n')
+        print('Converting int to str and concatenating:\n')
+        nextAttempt = 'varToConvert + str(convertedVar)'
+        print(nextAttempt)
+        print(eval(nextAttempt))
+    # Trying to add str value to int. Exception will be caught.
+    print('convertedVar + varToConvert')
+    try:
+        print(convertedVar + varToConvert)
+    except TypeError as error:
+        print('An error occurred :\n' + str(error))
+    finally:
+        print('\n')
+        print('Converting str to int and adding:\n')
+        nextAttempt = 'int(varToConvert) + convertedVar'
+        print(nextAttempt)
+        print(eval(nextAttempt))
     print('\n')
-    print('Converting int to str and concatenating:\n')
-    nextAttempt = 'varToConvert + str(convertedVar)'
-    print(nextAttempt)
-    print(eval(nextAttempt))
-# Trying to add str value to int. Exception will be caught.
-print('convertedVar + varToConvert')
-try:
-    print(convertedVar + varToConvert)
-except TypeError as error:
-    print('An error occurred :\n' + str(error))
-finally:
+
+def nullSafety(safe):
+    # Performs an operation on an unassigned variable, once with exception handling and once without
+    print('Safe = ' + str(safe))
+    # Null checking
+    # Null unsafe
+    if not safe :
+        print('With no exception handling, the program will crash')
+        b = 'Some value'
+        print(b)
+        b = b + c
+        print(b)
+    else:
+        # Null safe
+        print('Exception handling will ensure the program completes')
+        b = 'Some value'
+        print(b)
+        try:
+            b = b + c
+        except NameError as error:
+            print('An error occurred. The error message received was:\n' + str(error))
+        finally:
+            c = 'Some other value'
+            b = b + ' ' + c
+        print(b)
     print('\n')
-    print('Converting str to int and adding:\n')
-    nextAttempt = 'int(varToConvert) + convertedVar'
-    print(nextAttempt)
-    print(eval(nextAttempt))
 
-# Null checking
-# Null unsafe
-b = 'Some value'
-print(b)
-b = b + c
-print(b)
 
-# Null safe
-b = 'Some value'
-print(b)
-try:
-    b = b + c
-except NameError as error:
-    print('An error occurred. The error message received was:\n' + str(error))
-finally:
-    c = 'Some other value'
-    b = b + ' ' + c
-    print(b)
+varAssign()
+dataTypes()
+typeConversion()
+nullSafety(True)
+nullSafety(False)
