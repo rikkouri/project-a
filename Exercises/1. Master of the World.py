4c5198e4e70fbbfcb78bd6732ef6e1ee81d02756
Dave Davison <dave.davison@pgitl.com>
2016-01-18 13:28:25 +0000

    updating slides

---
+++ Exercises/1. Master of the World.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+# Exercise 1: Master of the Hello World
+
+name = raw_input("What is your name? ")
+
+print "Hello " + name
+
+home = raw_input("Where do you live?" )
+
+print "That's interesting!"
+
+print "Nice to meet you " + name + " from " + home
---
+++ Exercises/scratch.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+a = input("What is the value of a?")
+b = input("What is the value of b?")
+
+# Addition
+print a + b
+
+# Subtraction
+print a - b
+
+# Multiplication
+print a * b
+
+# Divison
+print a / b
+
+# Floor
+print a // b
+
+# Modulus
+print a % b
+
+# Expotential
+print a ** b
+
