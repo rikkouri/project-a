9bd8f0800f851915cb3ada4e11703093bd44e9b8
Paul Fox <paul.fox@pgitl.com>
2016-01-29 10:49:16 +0000

    Dictionaries exercise

---
+++ Exercises/Dictionaries.py
@@ -0,0 +1,7 @@
+# Set the values of the dictionary fields below from user input
+car = {'make': '', 'model': '', 'year': ''}
+
+# Then get the user to input values for colour, engine size and fuel type
+# and add the values to the car dictionary with appropriate keys
+
+# finally output the contents of the dictionary in a human-friendly style
